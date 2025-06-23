f04c3adf99a80049694b9504153b76a0f817d587
Seokju Hong <iamseokjuhong@gmail.com>
2025-06-23 20:54:18 +0900

    10. 5-1. 단일 책임 원칙 complete

--- object-principle-05-01/src/main/java/Schedule.java
+++ object-principle-05-01/src/main/java/Schedule.java
@@ -1,10 +1,3 @@
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -15,8 +8,8 @@ public class Schedule {
     private static final int DAYS_IN_WEEK = 7;
     
     private String title;
-    @JsonFormat(pattern = "HH:mm") private LocalTime from;
-    @JsonFormat(pattern = "MINUTES") private Duration duration;
+    LocalTime from;
+    private Duration duration;
     private Integer ordinalWeek;
     private DayOfWeek dayOfWeek;
 
@@ -34,14 +27,5 @@ public class Schedule {
         }
         
         return (day.getDayOfMonth() / DAYS_IN_WEEK) + 1 == ordinalWeek;
-    }
-    
-    public String toJson() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        
-        return mapper.writeValueAsString(this);
     }
 }
