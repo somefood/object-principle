3e62662846b4e93fb56ff9b1b313df24cc2f2214
Seokju Hong <iamseokjuhong@gmail.com>
2025-06-23 13:22:08 +0900

    4. 2-2. 테스트와 리팩터링

---
+++ object-principle-02-02/src/main/java/Room.java
@@ -0,0 +1,28 @@
+public class Room {
+    private int x, y;
+    private String name;
+    private String description;
+
+    public Room(int x, int y, String name, String description) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+}
