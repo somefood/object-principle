084060d8451c47cb8be04968584535c8d8120fa9
Seokju Hong <iamseokjuhong@gmail.com>
2025-06-23 21:08:27 +0900

    11. 5-2. 단일 책임 원칙을 위한 가이드 - 1차 분리

---
+++ object-principle-05-02/src/main/java/ScheduleJson.java
@@ -0,0 +1,28 @@
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ScheduleJson {
+    private Schedule schedule;
+    private ObjectMapper mapper;
+    
+    public ScheduleJson(Schedule schedule) {
+        this.schedule = schedule;
+        this.mapper = initializeMapper();
+    }
+
+    private ObjectMapper initializeMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        
+        return mapper;
+    }
+
+    public String toJson() throws JsonProcessingException {
+        return mapper.writeValueAsString(schedule);
+    }
+}
