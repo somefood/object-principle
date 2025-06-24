8bedb29f4b7297b1510ef5edee7f1f2357f69071
Seokju Hong <iamseokjuhong@gmail.com>
2025-06-24 10:16:51 +0900

    12. 5-3. 클래스 나누기 init

---
+++ object-principle-05-03/src/main/java/Game.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+
+public class Game {
+    private Size size;
+    private Room[] rooms;
+    private Position position;
+    private boolean running;
+
+    public Game() {
+        this.position = Position.of(0, 2);
+        this.size = Size.with(2, 3);
+        this.rooms = arrangeRooms(
+                new Room(Position.of(0, 0), "샘", "아름다운 샘물이 흐르는 곳입니다. 이곳에서 휴식을 취할 수 있습니다."),
+                new Room(Position.of(0, 1), "다리", "큰 강 위에 돌로 만든 커다란 다리가 있습니다."),
+                new Room(Position.of(1, 1), "성", "용왕이 살고 있는 성에 도착했습니다."),
+                new Room(Position.of(0, 2), "언덕", "저 멀리 성이 보이고 언덕 아래로 좁은 길이 나 있습니다."),
+                new Room(Position.of(1, 2), "동굴", "어둠에 잠긴 동굴 안에 작은 화톳불이 피어 있습니다."));
+    }
+
+    private Room[] arrangeRooms(Room ... rooms) {
+        Room[] result = new Room[size.area()];
+        for(var room : rooms) {
+            result[size.indexOf(room.position())] = room;
+        }
+        return result;
+    }
+
+    public void run() {
+        welcome();
+        play();
+        farewell();
+    }
+
+    private void welcome() {
+        showGreetings();
+        showRoom();
+        showHelp();
+    }
+
+    private void showGreetings() {
+        System.out.println("환영합니다!");
+    }
+
+    private void showRoom() {
+        System.out.println("당신은 [" + roomAt(position).name() + "]에 있습니다.");
+        System.out.println(roomAt(position).description());
+    }
+
+    private void showHelp() {
+        System.out.println("다음 명령어를 사용할 수 있습니다.");
+        System.out.println("go {north|east|south|west} - 이동, look - 보기, help - 도움말, quit - 게임 종료");
+    }
+
+    private void farewell() {
+        System.out.println("\n게임을 종료합니다.");
+    }
+
+    private void showBlocked() {
+        System.out.println("이동할 수 없습니다.");
+    }
+
+    private void play() {
+        Scanner scanner = new Scanner(System.in);
+
+        start();
+        while (isRunning()) {
+            String input = inputCommand(scanner);
+            parseCommand(input);
+        }
+    }
+
+    private String inputCommand(Scanner scanner) {
+        showPrompt();
+        return input(scanner);
+    }
+
+    private void start() {
+        running = true;
+    }
+    
+    private boolean isRunning() {
+        return running;
+    }
+
+    private void stop() {
+        this.running = false;
+    }
+
+    private void parseCommand(String input) {
+        String[] commands = input.toLowerCase().trim().split("\\s+");
+        switch (commands[0]) {
+            case "go" -> {
+                switch (commands[1]) {
+                    case "north" -> tryMove(Direction.NORTH);
+                    case "south" -> tryMove(Direction.SOUTH);
+                    case "east" -> tryMove(Direction.EAST);
+                    case "west" -> tryMove(Direction.WEST);
+                    default -> showUnknownCommand();
+                }
+            }
+            case "look" -> showRoom();
+            case "help" -> showHelp();
+            case "quit" -> stop();
+            default -> showUnknownCommand();
+        }
+    }
+
+    private void showUnknownCommand() {
+        System.out.println("이해할 수 없는 명령어입니다.");
+    }
+
+    private String input(Scanner scanner) {
+        return scanner.nextLine();
+    }
+
+    private void showPrompt() {
+        System.out.print("> ");
+    }
+
+    private void tryMove(Direction direction) {
+        if (isBlocked(position.shift(direction))) {
+            showBlocked();
+        } else {
+            this.position = position.shift(direction);
+            showRoom();
+        }
+    }
+
+    private boolean isBlocked(Position position) {
+        return isExcluded(position) || roomAt(position) == null;
+    }
+
+    private boolean isExcluded(Position position) {
+        return !size.contains(position);
+    }
+
+    private Room roomAt(Position position) {
+        return rooms[size.indexOf(position)];
+    }
+}
---
+++ object-principle-05-03/src/main/java/Position.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Position {
+    
+    private final int x;
+    private final int y;
+    
+    public static Position of(int x, int y) {
+        return new Position(x, y);
+    }
+
+    private Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Position shift(Direction direction) {
+        return switch (direction) {
+            case NORTH -> Position.of(x, y - 1);
+            case EAST -> Position.of(x + 1, y);
+            case SOUTH -> Position.of(x, y + 1);
+            case WEST -> Position.of(x - 1, y);
+        };
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+}
